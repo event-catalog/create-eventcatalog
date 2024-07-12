c1348d301f2c0aad2be58b8ffeaf3f3eaea0db2c
David Boyne <davidboyne@Davids-MBP.upp.com>
2024-07-12 11:34:46 +0100

    add public folder on new catalog

--- templates/default/eventcatalog.config.js
+++ templates/default/eventcatalog.config.js
@@ -1,14 +1,20 @@
 /** @type {import('@eventcatalog/core/bin/eventcatalog.config').Config} */
 export default {
   title: 'EventCatalog',
-  trailingSlash: false,
   tagline: 'Discover, Explore and Document your Event Driven Architectures',
   organizationName: 'Your Company',
   homepageLink: 'https://eventcatalog.dev/',
   editUrl: 'https://github.com/boyney123/eventcatalog-demo/edit/master',
+  // By default set to false, add true to get urls ending in /
+  trailingSlash: false,
+  // Change to make the base url of the site different, by default https://{website}.com/docs,
+  // changing to /company would be https://{website}.com/company/docs,
+  base: '/',
+  // Customize the logo, add your logo to public/ folder
   logo: {
     alt: 'EventCatalog Logo',
-    src: 'logo.svg'
+    src: 'logo.png',
+    text: 'EventCatalog'
   },
   docs: {
     sidebar: {
