75f37713070bb4d541a3ad3a026ad71c4683d1ff
David Boyne <boyneyy123@gmail.com>
2025-05-01 14:28:40 +0100

    feat(cli): added confluent template

---
+++ templates/confluent/eventcatalog.config.js
@@ -0,0 +1,45 @@
+/** @type {import('@eventcatalog/core/bin/eventcatalog.config').Config} */
+export default {
+  title: 'EventCatalog',
+  tagline: 'This internal platform provides a comprehensive view of our event-driven architecture across all systems. Use this portal to discover existing domains, explore services and their dependencies, and understand the message contracts that connect our infrastructure',
+  organizationName: '<organizationName>',
+  homepageLink: 'https://eventcatalog.dev/',
+  editUrl: 'https://github.com/boyney123/eventcatalog-demo/edit/master',
+  // By default set to false, add true to get urls ending in /
+  trailingSlash: false,
+  // Change to make the base url of the site different, by default https://{website}.com/docs,
+  // changing to /company would be https://{website}.com/company/docs,
+  base: '/',
+  // Customize the logo, add your logo to public/ folder
+  logo: {
+    alt: 'EventCatalog Logo',
+    src: '/logo.png',
+    text: 'EventCatalog'
+  },
+  // Enable RSS feed for your eventcatalog
+  rss: {
+    enabled: true,
+    // number of items to include in the feed per resource (event, service, etc)
+    limit: 20
+  },
+  docs: {
+    sidebar: {
+      // TREE_VIEW will render the DOCS as a tree view and map your file system folder structure
+      // LIST_VIEW will render the DOCS that look familiar to API documentation websites
+      type: 'LIST_VIEW'
+    },
+  },
+  // required random generated id used by eventcatalog
+  cId: '<cId>',
+  generators: [
+    [
+      '@eventcatalog/generator-confluent-schema-registry',
+      {
+        // The URL for your schema registry
+        schemaRegistryUrl: 'http://localhost:8081',
+        // This will document all the schemas in the registry, not just the latest version (default is false)
+        includeAllVersions: true
+      },
+    ],
+  ],
+}
